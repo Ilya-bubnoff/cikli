0bd2cf833fb68879915ee5d01319649930f97d43
ilyab <ilya.bubnoff@yandex.ru>
2025-01-26 22:48:54 +0300

    initial commit cikli

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World3!");
+        int savings=15000;
+        int total=0;
+        int i=1;
+        while(total < 245900) {
+            total= total+savings;
+            System.out.println("Месяц "+i+" ,сумма накоплений равна "+total+" рублей");
+            i++;}
+
+        int start = 1;
+        while(start<=10){
+            System.out.print(" "+start);
+            start++;}
+        System.out.println();
+        for (int a = 10; a > 0; a--){
+            System.out.print(" " + a);
+        }
     }
 }
